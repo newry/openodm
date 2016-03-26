20865781604cfcb317a2e0208183b5bcd2088d09
runiu <runiu@stubhub.com>
2016-03-25 22:51:53 -0700

    updated

--- impl/src/main/java/com/openodm/impl/entity/sdtm/SDTMVersion.java
+++ impl/src/main/java/com/openodm/impl/entity/sdtm/SDTMVersion.java
@@ -1,5 +1,12 @@
 package com.openodm.impl.entity.sdtm;
 
+import javax.persistence.OneToOne;
+
+import com.openodm.impl.entity.ct.CodeList;
+import com.openodm.impl.entity.ct.ControlTerminology;
+
 public class SDTMVersion {
+	@OneToOne(targetEntity = CodeList.class)
+	private ControlTerminology controlTerminology;
 
 }
